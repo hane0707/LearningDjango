630d8df0b1e24a568e65975c2cfea80c48355bcb
koob <hidamari4256@gmail.com>
2021-01-04 17:15:49 +0900

    about.html追加
    【手順 3-3:アプリにページを追加する】
    https://docs.microsoft.com/ja-jp/visualstudio/python/learn-django-in-visual-studio-step-03-serve-static-files-and-add-pages?view=vs-2019

--- BasicDjango/BasicDjango/urls.py
+++ BasicDjango/BasicDjango/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 import HelloDjangoApp.views
 
+# ルーティングの設定
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 ]
